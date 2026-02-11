859bc584141e8cc76534c740756c0a7877d7c19b
CoderATennis <arnavt777@gmail.com>
2026-02-11 11:30:45 -0500

    assignments updated

--- solution.py
+++ solution.py
@@ -1,1 +1,9 @@
-print("test")
+##(start node(variable), end_node(variable)) ** seperate each pair by a comma** -- for building 
+##for conditional probabilities (start_node, 1 or 0 depending on not for first node, 2nd node, 1 or 0 depending on not for 2nd node, probability of event happening)
+
+
+## 1 cause 1 result - arnav
+## 1 cause 2 results - audrey
+## 2 causes 1 result - audrey
+
+def build_final():
